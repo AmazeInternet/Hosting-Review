082daef8b410e8299359611dafdd23f3e8be5bc9
Satyam Kumar <Satyamdeveloper98@gmail.com>
2020-05-14 15:23:24 +0530

    Final Commit

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
@@ -5,9 +6,24 @@ navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
 
-document.getElementById("button-overlay").onclick = function () {
-  
-  document.getElementById("button-overlay").style.display = "none";
-  document.getElementById("button-hide").style.display = "none";
-  document.getElementById("revealed").style.display = "initial";
-}
+
+// document.getElementsByClassName("button-overlay").onclick = function () {
+
+//   // document.getElementById("button-overlay").style.display = "none";
+//   // document.getElementById("button-hide").style.display = "none";
+//   // document.getElementById("revealed").style.display = "initial";
+
+//   console.log("Hii");
+// }
+
+document.querySelectorAll('.button-overlay').forEach(item => {
+  item.addEventListener('click', event => {
+    var allBtn = document.querySelectorAll(".button-overlay.button-hide.revealed");
+
+    for (var i = 0; i <= allBtn.length; i++) {
+      document.querySelectorAll(".button-overlay")[i].style.display = "none";
+      document.querySelectorAll(".button-hide")[i].style.display = "none";
+      document.querySelectorAll(".revealed")[i].style.display = "initial";
+    };
+  });
+});
